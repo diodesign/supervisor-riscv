7dda53c13b2d35d7e675a877b8491828bda9a6e4
Chris Williams <diodesign@tuta.io>
2020-12-22 22:17:58 -0800

    Use stdout for reporting panics  * Also ensure EH frames are discarded as these have caused    build headaches in the past, flagged as unsupported by    the toolchain.

--- src/irq.rs
+++ src/irq.rs
@@ -5,33 +5,16 @@
  * See LICENSE for usage and copying.
  */
 
+#![allow(unused_must_use)]
+
+use core::fmt::Write;
 use riscv::register::scause;
 use riscv::register::sepc;
 
 #[no_mangle]
 pub extern "C" fn decode_irq()
 {
-    super::sbi::console_putchar('0' as u8);
-    super::sbi::console_putchar('x' as u8);
-    let chars = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' ];
-    let ptr = scause::read().bits();
-    for index in 1..17
-    {
-        let nibble = ptr >> (4 * (16 - index)) & 0xf;
-        super::sbi::console_putchar(chars[nibble] as u8);
-    }
-    super::sbi::console_putchar('\n' as u8);
-
-    super::sbi::console_putchar('0' as u8);
-    super::sbi::console_putchar('x' as u8);
-    let ptr = sepc::read();
-    for index in 1..17
-    {
-        let nibble = ptr >> (4 * (16 - index)) & 0xf;
-        super::sbi::console_putchar(chars[nibble] as u8);
-    }
-    super::sbi::console_putchar('\n' as u8);
-
-    // println!("Exception encountered: {:?} at {:x}", scause::read().cause(), sepc::read());
+    let mut stdout = super::stdio::Stdout::new();
+    write!(&mut stdout, "Exception encountered: {:?} at {:x}", scause::read().cause(), sepc::read());
     loop {}
 }
