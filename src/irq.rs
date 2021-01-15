ea4a404a52e2490a9d926c9ed3728084791694ef
Chris Williams <diodesign@tuta.io>
2021-01-15 04:44:17 -0800

    Protect exception handler with a lock

--- src/irq.rs
+++ src/irq.rs
@@ -7,14 +7,26 @@
 
 #![allow(unused_must_use)]
 
+use super::sbi;
 use core::fmt::Write;
 use riscv::register::scause;
 use riscv::register::sepc;
 
+lazy_static!
+{
+    static ref IRQ_LOCK: spin::Mutex<bool> = spin::Mutex::new(false);
+}
+
 #[no_mangle]
 pub extern "C" fn decode_irq()
 {
+    let lock = IRQ_LOCK.lock();
+
+    /* print out a message and shut down */
     let mut stdout = super::stdio::Stdout::new();
-    write!(&mut stdout, "Exception encountered: {:?} at {:x}", scause::read().cause(), sepc::read());
+    write!(&mut stdout, "Internal error: {:?} at 0x{:x}", scause::read().cause(), sepc::read());
+    sbi::shutdown(sbi::SBI_EXT_SYS_RESET_REASON::NoReason);
+
+    drop(lock);
     loop {}
 }
