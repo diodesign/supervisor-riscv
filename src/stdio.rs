500c6403e53a22bcb7f94e77015d9c5c59cc44df
Chris Williams <diodesign@tuta.io>
2021-02-23 02:20:06 -0800

    Support formatted output via console

--- src/stdio.rs
+++ src/stdio.rs
@@ -1,30 +1,69 @@
 /* diosix RV32G/RV64G supervisor-level code to input/output with the rest of the world
  *
- * (c) Chris Williams, 2020.
+ * (c) Chris Williams, 2020-2021.
  *
  * See LICENSE for usage and copying.
  */
 
+use spin::Mutex;
 use core::fmt;
 use super::sbi;
 
-/* simple object that writes out bytes to the user via SBI calls */
-pub struct Stdout;
-impl Stdout
+lazy_static!
 {
-    pub fn new() -> Stdout
-    {
-        Stdout {}
-    }
+    /* system standard output device */
+    pub static ref STDOUT: Mutex<Stdout> = Mutex::new(Stdout {});
 }
+
+/* print a formatted string to stdout with an automatic newline added */
+#[macro_export]
+macro_rules! println
+{
+    ($fmt:expr) => (stdout!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (stdout!(concat!($fmt, "\n"), $($arg)*));
+}
+
+/* print a formatted string to stdout without a newline added */
+#[macro_export]
+macro_rules! print
+{
+    ($fmt:expr) => (stdout!("{}", $fmt));
+    ($fmt:expr, $($arg:tt)*) => (stdout!(concat!($fmt), $($arg)*));
+}
+
+#[macro_export]
+macro_rules! stdout
+{
+    ($($arg:tt)*) =>
+    ({
+        use core::fmt::Write;
+        {
+            let mut lock = $crate::stdio::STDOUT.lock();
+            lock.write_fmt(format_args!($($arg)*)).unwrap();
+        }
+    });
+}
+
+pub struct Stdout;
 
 impl fmt::Write for Stdout
 {
     fn write_str(&mut self, s: &str) -> fmt::Result
     {
-        for c in s.bytes()
+        for c in s.chars()
         {
-            sbi::console_putchar(c);
+            loop
+            {
+                /* if putc() simply failed then try again. if it failed
+                for a specific reason (no permissions, not implemented, etc)
+                then bail out */
+                match sbi::putc(c)
+                {
+                    Ok(_) => break,
+                    Err(sbi::Error::Failed) => (),
+                    Err(_) => return Err(fmt::Error),
+                }
+            }
         }
         Ok(())
     }
